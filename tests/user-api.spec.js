d81460a27ec63db8724dd91954a697445d0f8240
Donia <doniagharbi141@gmail.com>
2026-06-15 17:11:27 +0100

    get api tests

--- tests/user-api.spec.js
+++ tests/user-api.spec.js
@@ -25,43 +25,43 @@ test('Get helpdesk list', async ({ request }) => {
     // Assert only 1 user is returned
     expect(Array.isArray(data)).toBeTruthy();
 });
-test('APi delete user successfully ', async ({ request }) => {
-    const response = await request.delete('http://localhost:8000/api/user/67ece430ab12130c7a2765fb')
-    expect(response.status()).toBe(200)
-    console.log(await response.json())
-})
-test('APi delete user not found ', async ({ request }) => {
-    const response = await request.delete('http://localhost:8000/api/user/6564b2f669a0f0354b8465fc')
-    expect(response.status()).toBe(400)
-    console.log(await response.json())
-})
-test('Update user data', async ({ request }) => {
-    const response = await request.patch('http://localhost:8000/api/user/6728a20d658392636ed94798', {
-        data: {
-            "firstname": "donia"
-        }
+// test('APi delete user successfully ', async ({ request }) => {
+//     const response = await request.delete('http://localhost:8000/api/user/67ece430ab12130c7a2765fb')
+//     expect(response.status()).toBe(200)
+//     console.log(await response.json())
+// })
+// test('APi delete user not found ', async ({ request }) => {
+//     const response = await request.delete('http://localhost:8000/api/user/6564b2f669a0f0354b8465fc')
+//     expect(response.status()).toBe(400)
+//     console.log(await response.json())
+// })
+// test('Update user data', async ({ request }) => {
+//     const response = await request.patch('http://localhost:8000/api/user/6728a20d658392636ed94798', {
+//         data: {
+//             "firstname": "donia"
+//         }
 
-    })
-    expect(response.status()).toBe(200)
-    const text = await response.text();
-    console.log(await response.json())
-})
-test('APi Post Request', async ({ request }) => {
-    const response = await request.post('http://localhost:8000/api/user/add', {
-        data: {
-            firstname: "test",
-            lastname: "leader",
-            email: "test@gmail.com",
-            password: "test123",
-            role: "MANAGER"
-        }
-    });
+//     })
+//     expect(response.status()).toBe(200)
+//     const text = await response.text();
+//     console.log(await response.json())
+// })
+// test('APi Post Request', async ({ request }) => {
+//     const response = await request.post('http://localhost:8000/api/user/add', {
+//         data: {
+//             firstname: "test",
+//             lastname: "leader",
+//             email: "test@gmail.com",
+//             password: "test123",
+//             role: "MANAGER"
+//         }
+//     });
 
-    const body = await response.text();
-    console.log('STATUS:', response.status());
-    console.log('BODY:', body);
-    expect(response.status()).toBe(200);
-});
+//     const body = await response.text();
+//     console.log('STATUS:', response.status());
+//     console.log('BODY:', body);
+//     expect(response.status()).toBe(200);
+// });
 
 
 
