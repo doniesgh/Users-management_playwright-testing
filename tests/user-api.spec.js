14f8bb4a21dee7a1df7001b67bae8c1a7a5c0484
Donia <doniagharbi141@gmail.com>
2026-06-15 16:58:55 +0100

    modify user api spec 2

--- tests/user-api.spec.js
+++ tests/user-api.spec.js
@@ -5,8 +5,12 @@ test('APi get Request', async ({ request }) => {
 
     expect(response.status()).toBe(200)
     const text = await response.text();
-    expect(text).toContain('Donia')
+    expect(text).toContain('test')
     console.log(await response.json());
+    const data = await response.json();
+
+    expect(Array.isArray(data)).toBeTruthy();
+    expect(data.length).toBeGreaterThan(0);
 
 
 })
@@ -19,22 +23,22 @@ test('Get helpdesk list', async ({ request }) => {
     console.log(data);
 
     // Assert only 1 user is returned
-    expect(data).toHaveLength(1);
+    expect(Array.isArray(data)).toBeTruthy();
 });
 test('APi delete user successfully ', async ({ request }) => {
-    const response = await request.delete('http://localhost:8000/api/user/67500a0a0232cf64724dba14')
+    const response = await request.delete('http://localhost:8000/api/user/67ece430ab12130c7a2765fb')
     expect(response.status()).toBe(200)
     console.log(await response.json())
 })
-// test('APi delete user not found ', async ({ request }) => {
-//     const response = await request.delete('http://localhost:8000/api/user/6564b2f669a0f0354b8465fc')
-//     expect(response.status()).toBe(400)
-//     console.log(await response.json())
-// })
+test('APi delete user not found ', async ({ request }) => {
+    const response = await request.delete('http://localhost:8000/api/user/6564b2f669a0f0354b8465fc')
+    expect(response.status()).toBe(400)
+    console.log(await response.json())
+})
 test('Update user data', async ({ request }) => {
-    const response = await request.patch('http://localhost:8000/api/user/67500a0a0232cf64724dba14', {
+    const response = await request.patch('http://localhost:8000/api/user/6728a20d658392636ed94798', {
         data: {
-            "firstname": "sabsouba"
+            "firstname": "donia"
         }
 
     })
